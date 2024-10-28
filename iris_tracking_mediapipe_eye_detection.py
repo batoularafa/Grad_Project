ed6de9f8ad5c7c6fcb67f1144f327ccfc0c34851
batoularafa <92258144+batoularafa@users.noreply.github.com>
2024-10-28 16:50:36 +0300

    Add files via upload

---
+++ iris_tracking_mediapipe_eye_detection.py
@@ -0,0 +1,73 @@
+import cv2 as cv
+import numpy as np
+import mediapipe as mp 
+import math
+
+# Left eyes indices 
+RIGHT_IRIS = [474,475,476,477]
+R_H_LEFT = [362]
+R_H_RIGHT = [263]
+# right eyes indices
+LEFT_IRIS = [469,470,471,472]
+L_H_LEFT = [33] #left eye left most landmark
+L_H_RIGHT =[133]
+
+def euclidean_distance(p1,p2):
+    x1, y1 = p1.ravel()
+    x2, y2 = p2.ravel()
+    distance = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+    return distance
+def eyes_pos(center, right, left):
+    center_to_right_dist = euclidean_distance(center, right)
+    total_distance = euclidean_distance(right, left)
+    ratio = center_to_right_dist/total_distance
+    iris_pos= ""
+    if ratio <= 0.42:
+        iris_pos = "right"
+    elif ratio > 0.42 and ratio <=0.57:
+        iris_pos = "center"
+    else:
+        iris_pos = "left"
+    return iris_pos, ratio
+
+mp_face_mesh = mp.solutions.face_mesh
+
+cap = cv.VideoCapture(0)
+with mp_face_mesh.FaceMesh(
+    max_num_faces=1, 
+    refine_landmarks=True, 
+    min_detection_confidence = 0.5,
+    min_tracking_confidence =0.5
+    )as face_mesh:
+    while True:
+        ret, frame = cap.read()
+        frame = cv.flip(frame,1)
+        rgb_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        img_h, img_w = frame.shape[:2]
+        results = face_mesh.process(rgb_frame)
+        if results.multi_face_landmarks:
+            # print(results.multi_face_landmarks[0].landmark )
+            # [print(p.x,p.y) for p in results.multi_face_landmarks[0].landmark] 
+            mesh_points = np.array([np.multiply([p.x,p.y], [img_w,img_h]).astype(int) for p in results.multi_face_landmarks[0].landmark] )
+            # print(mesh_points.shape)
+            # cv.polylines(frame, [mesh_points[LEFT_IRIS]], True, (0,255,0), 1, cv.LINE_AA)
+            # cv.polylines(frame, [mesh_points[RIGHT_IRIS]], True, (0,255,0), 1, cv.LINE_AA)
+            (l_cx, l_cy), l_radius = cv.minEnclosingCircle(mesh_points[LEFT_IRIS])
+            (r_cx, r_cy), r_radius = cv.minEnclosingCircle(mesh_points[RIGHT_IRIS])
+            center_left = np.array([l_cx, l_cy], dtype = np.int32)
+            center_right = np.array([r_cx, r_cy], dtype = np.int32)
+            cv.circle(frame, center_left, int(l_radius), (255,0,255), 1 , cv.LINE_AA)
+            cv.circle(frame, center_right, int(r_radius), (255,0,255), 1 , cv.LINE_AA)
+            cv.circle(frame, mesh_points[R_H_RIGHT][0], 3, (255,255,255), -1 , cv.LINE_AA)
+            cv.circle(frame, mesh_points[R_H_LEFT][0], 3, (0,255,255), -1 , cv.LINE_AA)
+            iris_position, ratio = eyes_pos(center_right, mesh_points[R_H_RIGHT], mesh_points[R_H_LEFT][0])
+            # print(iris_position)
+            cv.putText(frame,f'iris pos: {iris_position}, {ratio:.2f}', (30,30), cv.FONT_HERSHEY_PLAIN, 1.2, (0,255,0), 1, cv.LINE_AA)
+        cv.imshow('img', frame)
+        key = cv.waitKey(1)
+
+        if key == 27: #esc
+            break
+
+    cap.release()
+    cv.destroyAllWindows()

63479e5d1376337c02108694e91fd7726afad690
batoularafa <92258144+batoularafa@users.noreply.github.com>
2024-11-05 19:16:16 +0200

    Update iris_tracking_mediapipe_eye_detection.py
    detects two eyes and takes an average ratio

--- iris_tracking_mediapipe_eye_detection.py
+++ iris_tracking_mediapipe_eye_detection.py
@@ -2,6 +2,7 @@ import cv2 as cv
 import numpy as np
 import mediapipe as mp 
 import math
+import time
 
 # Left eyes indices 
 RIGHT_IRIS = [474,475,476,477]
@@ -24,10 +25,11 @@ def eyes_pos(center, right, left):
     iris_pos= ""
     if ratio <= 0.42:
         iris_pos = "right"
-    elif ratio > 0.42 and ratio <=0.57:
+    elif ratio > 0.42 and ratio < 0.57:
         iris_pos = "center"
-    else:
+    elif ratio >= 0.57:
         iris_pos = "left"
+
     return iris_pos, ratio
 
 mp_face_mesh = mp.solutions.face_mesh
@@ -60,9 +62,19 @@ with mp_face_mesh.FaceMesh(
             cv.circle(frame, center_right, int(r_radius), (255,0,255), 1 , cv.LINE_AA)
             cv.circle(frame, mesh_points[R_H_RIGHT][0], 3, (255,255,255), -1 , cv.LINE_AA)
             cv.circle(frame, mesh_points[R_H_LEFT][0], 3, (0,255,255), -1 , cv.LINE_AA)
-            iris_position, ratio = eyes_pos(center_right, mesh_points[R_H_RIGHT], mesh_points[R_H_LEFT][0])
-            # print(iris_position)
-            cv.putText(frame,f'iris pos: {iris_position}, {ratio:.2f}', (30,30), cv.FONT_HERSHEY_PLAIN, 1.2, (0,255,0), 1, cv.LINE_AA)
+            iris_position1, ratio1 = eyes_pos(center_right, mesh_points[R_H_RIGHT], mesh_points[R_H_LEFT][0])
+            iris_position2, ratio2 = eyes_pos(center_left, mesh_points[L_H_RIGHT], mesh_points[L_H_LEFT][0])
+            ratio = (ratio1+ratio2)/2
+            if ratio <= 0.45:
+                iris_pos = "right"
+            elif ratio > 0.45 and ratio < 0.55:
+                iris_pos = "center"
+            elif ratio >= 0.55:
+                iris_pos = "left"
+            print(iris_pos, ratio)
+            cv.putText(frame,f'iris pos: {iris_pos}, {ratio:.2f}', (30,30), cv.FONT_HERSHEY_PLAIN, 1.2, (0,255,0), 1, cv.LINE_AA)
+            # cv.putText(frame,f'iris pos: {iris_position2}, {ratio1:.2f}', (30,50), cv.FONT_HERSHEY_PLAIN, 1.2, (0,255,0), 1, cv.LINE_AA)
+            # print(iris_position2, ratio2)
         cv.imshow('img', frame)
         key = cv.waitKey(1)
 

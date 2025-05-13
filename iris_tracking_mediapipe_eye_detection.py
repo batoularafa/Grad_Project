aa91017ecb6a27b204d19fb67ccd4b23d04ef46f
batoul <batoarafa9@gmail.com>
2025-05-13 20:51:56 +0300

    serial write stop

--- iris_tracking_mediapipe_eye_detection.py
+++ iris_tracking_mediapipe_eye_detection.py
@@ -4,7 +4,7 @@ import mediapipe as mp
 import math
 import time
 
-#from picamera2 import Picamera2
+from picamera2 import Picamera2
 import serial
 
 # right eyes indices
@@ -24,15 +24,19 @@ LEFT_EYE_TOP = 386
 LEFT_EYE_BOTTOM = 374
 
 # start the picamera
-#picam2 = Picamera2()
-#picam2.preview_configuration.main.size = (800, 800)
-#picam2.preview_configuration.main.format = "RGB888"
-#picam2.preview_configuration.align()
-#picam2.configure("preview")
-#picam2.start()
+picam2 = Picamera2()
+picam2.preview_configuration.main.size = (800, 800)
+picam2.preview_configuration.main.format = "RGB888"
+picam2.preview_configuration.align()
+picam2.configure("preview")
+picam2.start()
 
 # ser = serial.Serial('COM7', 9600, timeout=1)
-# ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+
+def close_serial():
+    ser.write(b"5.0\n")  # Send stop command before closing
+    ser.close()
 
 def euclidean_distance(p1, p2):
     x1, y1 = p1.ravel()
@@ -66,7 +70,7 @@ def both_eye_blink(face_landmarks, frame, blink_threshold=0.02):
     )
 
     if left_eye_dist < blink_threshold and right_eye_dist < blink_threshold:
-        cv.putText(frame, "Blink Detected", (50, 70), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        # cv.putText(frame, "Blink Detected", (50, 70), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         print("Blink Detected")
         return True
     return False
@@ -83,14 +87,14 @@ def one_eye_blink(face_landmarks, frame, blink_threshold=0.02):
         np.array([right_eye_top.x, right_eye_top.y]) - np.array([right_eye_bottom.x, right_eye_bottom.y])
     )
     if ((left_eye_dist <= blink_threshold) and (right_eye_dist> blink_threshold)) or ((right_eye_dist <= blink_threshold) and (left_eye_dist > blink_threshold)):
-        cv.putText(frame, "One eyed Blink Detected", (50, 70), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        # cv.putText(frame, "One eyed Blink Detected", (50, 70), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         print("One eyed Blink Detected")
         return True
     return False
 
 mp_face_mesh = mp.solutions.face_mesh
 
-cap = cv.VideoCapture(0)
+# cap = cv.VideoCapture(0)
 
 wheel_state = False
 blink_flag = False
@@ -110,8 +114,8 @@ with mp_face_mesh.FaceMesh(
 ) as face_mesh:
     while True:
         current_time = time.time()
-        #frame = picam2.capture_array()
-        ret, frame = cap.read()
+        frame = picam2.capture_array()
+        # ret, frame = cap.read()
         frame = cv.flip(frame, 1)
         rgb_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
         img_h, img_w = frame.shape[:2]
@@ -121,16 +125,17 @@ with mp_face_mesh.FaceMesh(
             if wheel_state:  # only send stop if it was previously moving
                 wheel_state = False
                 speed=1
-                # ser.write(b"5.0\n")  
+                ser.write("5.0\n".encode())  
+                time.sleep(0.1)
                 print("Face not fully detected — stopping")
-                cv.putText(frame, "Face not detected - stopping", (30, 60), cv.FONT_HERSHEY_PLAIN, 1.2, (0, 0, 255), 1)
-            cv.imshow('img', frame)
-            key = cv.waitKey(1)
-            if key == 27:
-                break
+                # cv.putText(frame, "Face not detected - stopping", (30, 60), cv.FONT_HERSHEY_PLAIN, 1.2, (0, 0, 255), 1)
+            # cv.imshow('img', frame)
+            # key = cv.waitKey(1)
+            # if key == 27:
+            #     break
             continue  # Skip rest of the loop if no valid face
 
-        if results.multi_face_landmarks:
+        elif results.multi_face_landmarks:
             mesh_points = np.array(
                 [np.multiply([p.x, p.y], [img_w, img_h]).astype(int) for p in results.multi_face_landmarks[0].landmark])
 
@@ -146,22 +151,22 @@ with mp_face_mesh.FaceMesh(
                         blink_flag = True
                         wheel_state = not wheel_state
                     if blink_flag == True:
-                        cv.putText(frame, "toggle done", (30, 30), cv.FONT_HERSHEY_PLAIN, 1.2, (0, 255, 0), 1, cv.LINE_AA)
+                        # cv.putText(frame, "toggle done", (30, 30), cv.FONT_HERSHEY_PLAIN, 1.2, (0, 255, 0), 1, cv.LINE_AA)
                         print("toggle done")
                     iris_positions_blink = [pos for pos in iris_positions if blink_time - pos[1] <= 1]
                     iris_ratios_blink = [pos for pos in iris_ratios if blink_time - pos[1] <= 1]
                     if iris_positions_blink and wheel_state == True:
                         last_iris_position, _ = iris_positions_blink[-1]
                         last_iris_ratio, _ = iris_ratios_blink[-1]
-                        cv.putText(frame, f'Speed: {speed}', (30, 120), cv.FONT_HERSHEY_PLAIN, 1.2, (255, 0, 255), 1)
-                        cv.putText(frame, f'iris pos: {last_iris_position}, {last_iris_ratio:.2f}', (30, 30), cv.FONT_HERSHEY_PLAIN, 1.2, (0, 255, 0), 1, cv.LINE_AA)
+                        # cv.putText(frame, f'Speed: {speed}', (30, 120), cv.FONT_HERSHEY_PLAIN, 1.2, (255, 0, 255), 1)
+                        # cv.putText(frame, f'iris pos: {last_iris_position}, {last_iris_ratio:.2f}', (30, 30), cv.FONT_HERSHEY_PLAIN, 1.2, (0, 255, 0), 1, cv.LINE_AA)
                         print(f'Speed: {speed}', f'iris pos: {last_iris_position}, {last_iris_ratio:.2f}')
-                        #ser.write(f"{last_iris_ratio}\n".encode())
-                        # if speed == 1:
-                        #     ser.write(f"s1@{last_iris_ratio}\n".encode())
-                        # else:
-                        #     ser.write(f"s2@{last_iris_ratio}\n".encode())
-                        # time.sleep(0.1)
+                        
+                        if speed == 1:
+                            ser.write(f"s1@{last_iris_ratio}\n".encode())
+                        elif speed == 2:
+                            ser.write(f"s2@{last_iris_ratio}\n".encode())
+                        time.sleep(0.1)
                         # if ser.in_waiting > 0:
                         #     data = ser.readline().decode('utf-8').strip()
                         #     print(f"Received: {data}")
@@ -176,26 +181,26 @@ with mp_face_mesh.FaceMesh(
                         one_eye_blink_flag = True
 
                     if one_eye_blink_flag:
-                        cv.putText(frame, "Speed Toggle", (30, 30), cv.FONT_HERSHEY_PLAIN, 1.2, (0, 255, 0), 1, cv.LINE_AA)
+                        # cv.putText(frame, "Speed Toggle", (30, 30), cv.FONT_HERSHEY_PLAIN, 1.2, (0, 255, 0), 1, cv.LINE_AA)
                         print("Speed Toggle")
                     one_iris_positions_blink = [pos for pos in iris_positions if one_eye_closed_time - pos[1] <= 1]
                     one_iris_ratios_blink = [pos for pos in iris_ratios if one_eye_closed_time - pos[1] <= 1]
                     if one_iris_ratios_blink:
                         last_iris_position, _ = one_iris_positions_blink[-1]
                         last_iris_ratio, _ = one_iris_ratios_blink[-1]
-                        cv.putText(frame, f'Speed: {speed}', (30, 120), cv.FONT_HERSHEY_PLAIN, 1.2, (255, 0, 255), 1)
-                        cv.putText(frame, f'iris pos: {last_iris_position}, {last_iris_ratio:.2f}', (30, 30),cv.FONT_HERSHEY_PLAIN, 1.2, (0, 255, 0), 1, cv.LINE_AA)
+                        # cv.putText(frame, f'Speed: {speed}', (30, 120), cv.FONT_HERSHEY_PLAIN, 1.2, (255, 0, 255), 1)
+                        # cv.putText(frame, f'iris pos: {last_iris_position}, {last_iris_ratio:.2f}', (30, 30),cv.FONT_HERSHEY_PLAIN, 1.2, (0, 255, 0), 1, cv.LINE_AA)
                         print(f'Speed: {speed}', f'iris pos: {last_iris_position}, {last_iris_ratio:.2f}')
-                        #ser.write(f"{last_iris_ratio}\n".encode())
-                        # if speed == 1:
-                        #     ser.write(f"s1@{last_iris_ratio}\n".encode())
-                        # else:
-                        #     ser.write(f"s2@{last_iris_ratio}\n".encode())
-                        # time.sleep(0.1)
+                       
+                        if speed == 1:
+                            ser.write(f"s1@{last_iris_ratio}\n".encode())
+                        elif speed == 2:
+                            ser.write(f"s2@{last_iris_ratio}\n".encode())
+                        time.sleep(0.1)
                         # if ser.in_waiting > 0:
                         #     data = ser.readline().decode('utf-8').strip()
                         #     print(f"Received: {data}")
-                        # time.sleep(0.1)
+                        # #time.sleep(0.1)
                     
             else:
                 blink_time = 0
@@ -221,41 +226,41 @@ with mp_face_mesh.FaceMesh(
                 iris_ratios.append((avg_ratio, time.time()))
 
                 if wheel_state == True:
-                    # if speed == 1:
-                    #     ser.write(f"s1@{avg_ratio}\n".encode())
-                    # else:
-                    #     ser.write(f"s2@{avg_ratio}\n".encode())
-                    # time.sleep(0.1)
+                    if speed == 1:
+                        ser.write(f"s1@{avg_ratio}\n".encode())
+                    elif speed == 2:
+                        ser.write(f"s2@{avg_ratio}\n".encode())
+                    time.sleep(0.1)
                     # if ser.in_waiting > 0:
                     #     data = ser.readline().decode('utf-8').strip()
                     #     print(f"Received: {data}")
-                    # time.sleep(0.1)
+                    # #time.sleep(0.1)
 
-                    cv.putText(frame, "moving", (30, 60), cv.FONT_HERSHEY_PLAIN, 1.2, (0, 255, 0), 1, cv.LINE_AA)
-                    cv.putText(frame, f'Speed: {speed}', (30, 120), cv.FONT_HERSHEY_PLAIN, 1.2, (255, 0, 255), 1)
-                    cv.putText(frame, f'iris pos: {iris_pos}, {avg_ratio:.2f}', (30, 30), cv.FONT_HERSHEY_PLAIN, 1.2, (0, 255, 0), 1, cv.LINE_AA)
+                    # cv.putText(frame, "moving", (30, 60), cv.FONT_HERSHEY_PLAIN, 1.2, (0, 255, 0), 1, cv.LINE_AA)
+                    # cv.putText(frame, f'Speed: {speed}', (30, 120), cv.FONT_HERSHEY_PLAIN, 1.2, (255, 0, 255), 1)
+                    # cv.putText(frame, f'iris pos: {iris_pos}, {avg_ratio:.2f}', (30, 30), cv.FONT_HERSHEY_PLAIN, 1.2, (0, 255, 0), 1, cv.LINE_AA)
                     print("moving", f'Speed: {speed}', f'iris pos: {iris_pos}, {avg_ratio:.2f}')
 
-                else:
+                elif wheel_state == False:
                     one_eye_closed_time = 0
                     speed =1
-                    # ser.write(f"5.0\n".encode())
-                    # time.sleep(0.1)
+                    ser.write("5.0\n".encode())
+                    time.sleep(0.1)
                     # if ser.in_waiting > 0:
                     #     data = ser.readline().decode('utf-8').strip()
                     #     print(f"Received: {data}")
-                    # time.sleep(0.1)
-                    cv.putText(frame, "stopping", (30, 60), cv.FONT_HERSHEY_PLAIN, 1.2, (0, 255, 0), 1, cv.LINE_AA)
-                    cv.putText(frame, f'Speed: {speed}', (30, 120), cv.FONT_HERSHEY_PLAIN, 1.2, (255, 0, 255), 1)
-                    cv.putText(frame, f'iris pos: {iris_pos}, {avg_ratio:.2f}', (30, 30), cv.FONT_HERSHEY_PLAIN, 1.2,(0, 255, 0), 1, cv.LINE_AA)
+                    # #time.sleep(0.1)
+                    # cv.putText(frame, "stopping", (30, 60), cv.FONT_HERSHEY_PLAIN, 1.2, (0, 255, 0), 1, cv.LINE_AA)
+                    # cv.putText(frame, f'Speed: {speed}', (30, 120), cv.FONT_HERSHEY_PLAIN, 1.2, (255, 0, 255), 1)
+                    # cv.putText(frame, f'iris pos: {iris_pos}, {avg_ratio:.2f}', (30, 30), cv.FONT_HERSHEY_PLAIN, 1.2,(0, 255, 0), 1, cv.LINE_AA)
                     print("stopping", f'iris pos: {iris_pos}, {avg_ratio:.2f}')
 
-        cv.imshow('img', frame)
-        key = cv.waitKey(1)
-        if key == 27:
-            break
+        # cv.imshow('img', frame)
+        # key = cv.waitKey(1)
+        # if key == 27:
+        #     break
 
-    cap.release()
-    # ser.write(b"5.0\n")
-    # time.sleep(0.1)
-    cv.destroyAllWindows()
+    # cap.release()
+    ser.write("5.0\n".encode())
+    time.sleep(0.1)
+    # cv.destroyAllWindows()
